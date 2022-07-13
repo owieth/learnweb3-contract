f4fee068889a3849cacde75d416331f0db87c7aa
owieth <60756360+owieth@users.noreply.github.com>
2022-07-13 18:51:16 +0200

    feat(senior): add merkle tree contract

---
+++ senior/MerkleTree.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/// @title Contract for https://www.learnweb3.io/tracks/senior/merkle-trees
+/// @author Olivier Winkler
+/// @dev All function calls are currently implemented without side effects
+contract MerkleTree {
+    bytes32 public merkleRoot;
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
+
+    function checkInWhitelist(
+        bytes32[] calldata proof,
+        uint64 maxAllowanceToMint
+    ) public view returns (bool) {
+        bytes32 leaf = keccak256(abi.encode(msg.sender, maxAllowanceToMint));
+        bool verified = MerkleProof.verify(proof, merkleRoot, leaf);
+        return verified;
+    }
+}
