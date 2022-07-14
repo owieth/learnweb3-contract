2aa159f2616fe7ebb8ffc1d2d2cd81383ad6e600
owieth <60756360+owieth@users.noreply.github.com>
2022-07-14 17:09:26 +0200

    feat(senior): add upgradeable contracts

---
+++ senior/Upgradeable.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+/// @title Contract for https://www.learnweb3.io/tracks/senior/upgradeable-contracts
+/// @author Olivier Winkler
+/// @dev All function calls are currently implemented without side effects
+contract Upgradeable is
+    Initializable,
+    ERC721Upgradeable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
+    // Note how we created an initialize function and then added the
+    // initializer modifier which ensure that the
+    // initialize function is only called once
+    function initialize() public initializer {
+        // Note how instead of using the ERC721() constructor, we have to manually initialize it
+        // Same goes for the Ownable contract where we have to manually initialize it
+        __ERC721_init("LW3NFT", "LW3NFT");
+        __Ownable_init();
+        _mint(msg.sender, 1);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+}
